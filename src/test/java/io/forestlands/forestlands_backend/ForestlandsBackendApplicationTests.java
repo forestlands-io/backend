fce9a7f65492012ce8cb27d43738563fc80f9085
sdrahan <serhii.drahan@raisin.com>
2025-10-20 17:49:43 +0200

    Initial commit

---
+++ src/test/java/io/forestlands/forestlands_backend/ForestlandsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.forestlands.forestlands_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestlandsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
