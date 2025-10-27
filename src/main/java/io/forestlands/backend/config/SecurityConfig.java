d62819cb5b813179130c83b32a3aca7ab9d21ff0
sdrahan <serhii.drahan@raisin.com>
2025-10-27 16:32:26 +0100

    User Argon2 instead of BCrypt

--- src/main/java/io/forestlands/backend/config/SecurityConfig.java
+++ src/main/java/io/forestlands/backend/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -35,6 +35,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new Argon2PasswordEncoder(16, 32, 1, 65536, 3);
     }
 }
