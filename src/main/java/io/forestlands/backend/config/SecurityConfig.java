54816cab2e2d9a7136ab4a460cc85db08fc4bd6c
sdrahan <serhii.drahan@raisin.com>
2025-10-27 12:01:47 +0100

    Add springdoc

--- src/main/java/io/forestlands/backend/config/SecurityConfig.java
+++ src/main/java/io/forestlands/backend/config/SecurityConfig.java
@@ -13,11 +13,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs/**",
+            "/v3/api-docs.yaml",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
                         .requestMatchers("/api/v1/users/login").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
